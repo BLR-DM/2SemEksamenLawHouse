e65d8f0e996a6f8d98f6011eb29f93a11b061df3
Bilal Kinali <biki59986@edu.ucl.dk>
2024-04-29 21:21:17 +0200

    added some functionality to loginpage

---
+++ UI/Forms/CreateUserPage/CreateUserView.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using UI.Forms.LoginPage;
+
+namespace UI.Forms.CreateUserPage
+{
+    public partial class CreateUserView : Form
+    {
+        LoginPageView loginPage;
+        public CreateUserView(LoginPageView login)
+        {
+            loginPage = login;
+            InitializeComponent();
+        }
+
+        protected override void OnFormClosing(FormClosingEventArgs e)
+        {
+            loginPage.Show();
+        }
+    }
+}

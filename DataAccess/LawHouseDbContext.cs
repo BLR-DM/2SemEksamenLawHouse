3ea30ca8beb5ae9a07b3e2456cfcf05bcc49e7f2
Bilal Kinali <biki59986@edu.ucl.dk>
2024-04-30 13:35:49 +0200

    server

--- DataAccess/LawHouseDbContext.cs
+++ DataAccess/LawHouseDbContext.cs
@@ -24,7 +24,8 @@ namespace DataAccess
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             //optionsBuilder.UseSqlServer("Server=mssql16.unoeuro.com;Database=lawhouseblr_dk_db_lawhouse;User Id=lawhouseblr_dk;Password=km5xFBGRe2pErnDcg6h3;Encrypt=False;");
-            optionsBuilder.UseSqlServer("Server=DESKTOP-ANPNVL3;DataBase=Dinfar;Trusted_Connection=true;Encrypt=False;");
+            //optionsBuilder.UseSqlServer("Server=DESKTOP-ANPNVL3;DataBase=Dinfar;Trusted_Connection=true;Encrypt=False;");
+            optionsBuilder.UseSqlServer("Server=BILAL-KINALI;Database=LawHouseTest2;Trusted_Connection=True;Encrypt=false;");
         }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {

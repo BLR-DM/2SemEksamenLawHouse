6cdfa42ecc943da11953046f1782e5964bb87014
ZmolZito <maclucas07@hotmail.com>
2024-05-03 22:14:08 +0200

    lol

--- UI/Program.cs
+++ UI/Program.cs
@@ -21,14 +21,14 @@ namespace UI
 
             //Lawyer lawyer = new Lawyer()
             //{
-                FirstName = "Bilal",
-                LastName = "Kinali",
-                Email = "bilalkinali@gmail.com",
-                AddressLine = "Lumbyesvej 16",
-                PostalCode = 7000,
-                PhoneNumber = 88888888,
-                HireDate = DateTime.Now,
-                LawyerTitleID = 1,
+                //FirstName = "Bilal",
+                //LastName = "Kinali",
+                //Email = "bilalkinali@gmail.com",
+                //AddressLine = "Lumbyesvej 16",
+                //PostalCode = 7000,
+                //PhoneNumber = 88888888,
+                //HireDate = DateTime.Now,
+                //LawyerTitleID = 1,
 
             //    LoginDetails = new LoginDetails()
             //    {

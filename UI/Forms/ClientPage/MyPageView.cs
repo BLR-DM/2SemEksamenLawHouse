574fcc0a498f54331ae18505f64d9dbad1ba9f82
ZmolZito <maclucas07@hotmail.com>
2024-05-05 00:15:25 +0200

    MyPage tilføjet

---
+++ UI/Forms/ClientPage/MyPageView.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UI.Forms.ClientPage
+{
+    public partial class MyPageView : Form
+    {
+        public MyPageView()
+        {
+            InitializeComponent();
+            lblMustSubscribe.Text = "        Subscribe\n To use calculations";
+        }
+    }
+}

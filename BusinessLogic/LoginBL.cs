1b00443c9337f736ec0d034d27b18090a3cd9e74
Bilal Kinali <biki59986@edu.ucl.dk>
2024-05-05 13:35:58 +0200

    login

---
+++ BusinessLogic/LoginBL.cs
@@ -0,0 +1,33 @@
+﻿using DataAccess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccess;
+using EntityModels;
+
+namespace BusinessLogic
+{
+    public class LoginBL
+    {
+        LoginDbAccess dbAccess;
+        ModelConverter modelConverter;
+
+        public LoginBL()
+        {
+            dbAccess = new LoginDbAccess();
+            modelConverter = new ModelConverter();
+        }
+
+        public async Task<bool> CheckUsernameAsync(string username)
+        {
+            return await dbAccess.CheckUsernameAsync(username);
+        }
+
+        public async Task<LoginDetails> CheckUsernameAndPasswordAsync(string username, string password)
+        {
+            return await dbAccess.CheckUsernameAndPasswordAsync(username, password);
+        }
+    }
+}

d3c2ba709173d2508cb6bebbfa446d45510cb28d
ZmolZito <maclucas07@hotmail.com>
2024-05-06 01:53:39 +0200

    Jeow

--- UI/Forms/ClientPage/ClientsView.cs
+++ UI/Forms/ClientPage/ClientsView.cs
@@ -14,33 +14,74 @@ namespace UI.Forms.ClientPage
 {
     public partial class ClientsView : Form
     {
-        BindingSource ClientsBS;
-        List<ClientUI> clients;
+        List<ClientUI> originalClientsList;
+        List<ClientUI> filteredClients;
         ClientBL clientBL;
+        int count;
         public ClientsView()
         {
             InitializeComponent();
             clientBL = new ClientBL();
-            ClientsBS = new BindingSource();
 
-            Load += ClientsView_Load;            
+            Load += ClientsView_Load;
+            txtSearchPhone.TextChanged += TxtSearchPhone_TextChanged;
+            txtSearchPostal.TextChanged += TxtSearchPostal_TextChanged;
         }
 
-        private void ClientsView_Load(object? sender, EventArgs e)
+        private void TxtSearchPostal_TextChanged(object? sender, EventArgs e)
         {
-            dgvClients.DataSource = ClientsBS;
-
-            LoadClientsAsync();
+            SortData();
         }
 
-        public async Task LoadClientsAsync()
+        private void TxtSearchPhone_TextChanged(object? sender, EventArgs e)
         {
-            clients = await clientBL.GetClientsAsync();
-            ClientsBS.DataSource = clients;
+            SortData();
+        }
+
+        private void SortData()
+        {
+            filteredClients = new List<ClientUI>(originalClientsList);
+
+
+            if (!string.IsNullOrEmpty(txtSearchPhone.Text) && int.TryParse(txtSearchPhone.Text, out int phonenumber))
+            {
+                filteredClients = filteredClients.Where(client => client.MainPhone.ToString().StartsWith(phonenumber.ToString())).ToList();
+                dgvClients.DataSource = filteredClients;
+            }
+            if (!string.IsNullOrEmpty(txtSearchPostal.Text) && int.TryParse(txtSearchPostal.Text, out int postalcode))
+            {
+                filteredClients = filteredClients.Where(client => client.PostalCode.ToString().StartsWith(postalcode.ToString())).ToList();
+                dgvClients.DataSource = filteredClients;
+            }
+
+            dgvClients.DataSource = filteredClients;
+
+            count = filteredClients.Count;
+            lblClientAmmount.Text = count.ToString() + " Clients";
+        }
+
+        private async void ClientsView_Load(object? sender, EventArgs e)
+        {
+            originalClientsList = await clientBL.GetClientsAsync();
+            filteredClients = new List<ClientUI>(originalClientsList);
+            dgvClients.DataSource = filteredClients;
+            count = filteredClients.Count;
+            lblClientAmmount.Text = count.ToString() + " Clients";
+
 
             DGVSetup();
-
+            //LoadClientsAsync();
         }
+
+        //public async Task LoadClientsAsync()
+        //{
+        //    clients = await clientBL.GetClientsAsync();
+        //    ClientsBS.DataSource = clients;
+        //    filteredClients = new List<ClientUI>(clients);
+
+        //    DGVSetup();
+
+        //}
         private void DGVSetup()
         {
 

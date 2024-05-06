52c9deafa48ef4e9025d26e35e79e910d3b29a22
Bilal Kinali <biki59986@edu.ucl.dk>
2024-05-06 03:17:47 +0200

    validation not finished

--- UI/Forms/LoginPage/CreateUserView.cs
+++ UI/Forms/LoginPage/CreateUserView.cs
@@ -10,7 +10,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using UI.Forms.LoginPage;
-using Microsoft.IdentityModel.Tokens;
+using BusinessLogic.Validation;
 
 namespace UI.Forms.CreateUserPage
 {
@@ -18,9 +18,18 @@ namespace UI.Forms.CreateUserPage
     {
         ClientBL clientBL;
         List<PhoneUI> phoneUIs;
+        PersonValidator pValidator;
+        Color rgbColorBlue;
+        Color validFormat;
+        Color invalidFormat;
         public CreateUserView()
         {
             clientBL = new ClientBL();
+            pValidator = new PersonValidator();
+
+            rgbColorBlue = Color.FromArgb(45, 93, 134);
+            validFormat = Color.Black;
+            invalidFormat = Color.OrangeRed;
 
             InitializeComponent();
 
@@ -28,15 +37,113 @@ namespace UI.Forms.CreateUserPage
             lblCancel.MouseHover += LblCancel_MouseHover;
             lblCancel.MouseLeave += LblCancel_MouseLeave;
             pboxEye.Click += PboxEye_Click;
-            txtEmail.TextChanged += TxtEmail_TextChanged;
-            txtEmailConfirm.TextChanged += TxtEmailConfirm_TextChanged;
             btnCreate.Click += BtnCreate_Click;
+
+            // Validerings events
+            txtFirstname.TextChanged += TxtFirstname_TextChanged;
+            txtLastname.TextChanged += TxtLastname_TextChanged;
+            txtEmail.Leave += TxtEmail_Leave;
+            txtEmailConfirm.Leave += TxtEmailConfirm_Leave;
+            txtPhoneMain.Leave += TxtPhoneMain_Leave;
+            txtPhoneAlt.Leave += TxtPhoneAlt_Leave;
+            txtAddress.TextChanged += TxtAddress_TextChanged;
+            txtPostal.Leave += TxtPostal_Leave;
+            txtPassword.TextChanged += TxtPassword_TextChanged;
+            txtPasswordConfirm.TextChanged += TxtPasswordConfirm_TextChanged;
+            txtPassword.GotFocus += TxtPassword_GotFocus;
+        }
+
+        private void TxtPassword_GotFocus(object? sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void TxtPasswordConfirm_TextChanged(object? sender, EventArgs e)
+        {
+            txtPasswordConfirm.ForeColor = pValidator.ValidPassword(txtPasswordConfirm.Text) ? validFormat : invalidFormat;
+        }
+
+        private void TxtPassword_TextChanged(object? sender, EventArgs e)
+        {
+            txtPassword.ForeColor = pValidator.ValidPassword(txtPassword.Text) ? validFormat : invalidFormat;
+        }
+
+        private void TxtPostal_Leave(object? sender, EventArgs e)
+        {
+            txtPostal.ForeColor = pValidator.ValidPostalCode(txtPostal.Text) ? validFormat : invalidFormat;
+        }
+
+        private void TxtAddress_TextChanged(object? sender, EventArgs e)
+        {
+            txtAddress.ForeColor = pValidator.ValidAddress(txtAddress.Text) ? validFormat : invalidFormat;
+        }
+
+        private void TxtPhoneAlt_Leave(object? sender, EventArgs e)
+        {
+            txtPhoneAlt.ForeColor = pValidator.ValidPhone(txtPhoneAlt.Text) ? validFormat : invalidFormat;
+        }
+
+        private void TxtPhoneMain_Leave(object? sender, EventArgs e)
+        {
+            txtPhoneMain.ForeColor = pValidator.ValidPhone(txtPhoneMain.Text) ? validFormat : invalidFormat;
+        }
+
+        private void TxtEmailConfirm_Leave(object? sender, EventArgs e)
+        {
+            if (pValidator.ValidEmail(txtEmailConfirm.Text))
+            {
+                txtEmailConfirm.ForeColor = validFormat;
+
+                if (txtEmailConfirm.Text == txtEmail.Text)
+                {
+                    txtUsername.Text = txtEmailConfirm.Text;
+                    txtEmailConfirm.ForeColor = txtEmail.ForeColor = validFormat;
+                }
+                else
+                {
+                    txtUsername.Text = string.Empty;
+                    txtEmailConfirm.ForeColor = txtEmail.ForeColor = invalidFormat;
+                }
+            }
+            else
+            {
+                txtEmailConfirm.ForeColor = invalidFormat;
+            }
+        }
+
+        private void TxtEmail_Leave(object? sender, EventArgs e)
+        {
+            if (pValidator.ValidEmail(txtEmail.Text))
+            {
+                txtEmail.ForeColor = validFormat;
+
+                if (txtEmailConfirm.Text == txtEmail.Text)
+                {
+                    txtUsername.Text = txtEmail.Text;
+                }
+                else
+                {
+                    txtUsername.Text = string.Empty;
+                }
+            }
+            else
+            {
+                txtEmail.ForeColor = invalidFormat;
+            }
+        }
+
+        private void TxtLastname_TextChanged(object? sender, EventArgs e)
+        {
+            txtLastname.ForeColor = pValidator.ValidName(txtLastname.Text) ? validFormat : invalidFormat;
+        }
+
+        private void TxtFirstname_TextChanged(object? sender, EventArgs e)
+        {
+            txtFirstname.ForeColor = pValidator.ValidName(txtFirstname.Text) ? validFormat : invalidFormat;
         }
 
         private async void BtnCreate_Click(object? sender, EventArgs e)
         {
-            phoneUIs = new List<PhoneUI>();
-
             // Opret Client UI
             ClientUI client = new ClientUI()
             {
@@ -56,13 +163,15 @@ namespace UI.Forms.CreateUserPage
                 CreationDate = DateTime.Now,
             };
 
+            phoneUIs = new List<PhoneUI>();
+
             // Opret Phone UI
             PhoneUI phoneUI = new PhoneUI { PhoneNumber = int.Parse(txtPhoneMain.Text) };
 
             phoneUIs.Add(phoneUI);
 
             // Opret PhoneAlt UI
-            if (!txtPhoneAlt.Text.IsNullOrEmpty())
+            if (!string.IsNullOrWhiteSpace(txtPhoneAlt.Text))
             {
                 PhoneUI phoneUIAlt = new PhoneUI { PhoneNumber = int.Parse(txtPhoneAlt.Text) };
                 phoneUIs.Add(phoneUIAlt);
@@ -76,31 +185,6 @@ namespace UI.Forms.CreateUserPage
                 MessageBox.Show("Failed!");
         }
 
-        private void TxtEmailConfirm_TextChanged(object? sender, EventArgs e)
-        {
-            // Husk validering
-            if (txtEmail.Text == txtEmailConfirm.Text)
-            {
-                txtUsername.Text = txtEmailConfirm.Text;
-            }
-            else
-            {
-                txtUsername.Text = "";
-            }
-        }
-
-        private void TxtEmail_TextChanged(object? sender, EventArgs e)
-        {
-            // Husk validering
-            if (txtEmail.Text == txtEmailConfirm.Text)
-            {
-                txtUsername.Text = txtEmailConfirm.Text;
-            }
-            else
-            {
-                txtUsername.Text = "";
-            }
-        }
 
         private void LblCancel_MouseHover(object? sender, EventArgs e)
         {

7b8c6ad4bb4b5297571ebb6003e3b584b8294ba1
Bilal Kinali <biki59986@edu.ucl.dk>
2024-05-07 00:56:03 +0200

    .

---
+++ UI/Forms/ClientPage/EmployeesView.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using BusinessLogic;
+using UIModels;
+
+namespace UI.Forms.ClientPage
+{
+    public partial class EmployeesView : Form
+    {
+        LawyerBL lawyerBL;
+        List<LawyerUI> lawyerUIs;
+        public EmployeesView()
+        {
+            lawyerBL = new LawyerBL();
+            lawyerUIs = new List<LawyerUI>();
+
+            InitializeComponent();
+
+            Load += EmployeesView_Load;
+        }
+
+        private void EmployeesView_Load(object? sender, EventArgs e)
+        {
+            
+        }
+    }
+}

6619d1db8ccb199ad738b8dc3cc7c3cabe469be0
ZmolZito <maclucas07@hotmail.com>
2024-05-13 22:37:50 +0200

    grr

---
+++ UI/Forms/Self-Service/CalculationsView.Designer.cs
@@ -0,0 +1,47 @@
+﻿namespace UI.Forms.Self_Service
+{
+    partial class CalculationsView
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            SuspendLayout();
+            // 
+            // CalculationsView
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.GradientInactiveCaption;
+            ClientSize = new Size(998, 613);
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "CalculationsView";
+            Text = "CalculationsView";
+            ResumeLayout(false);
+        }
+
+        #endregion
+    }
+}

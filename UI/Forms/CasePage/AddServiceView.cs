0b6d96218f2d2e7bc0ca81f41d81b2e74a9c22f8
skovbov <skovbov@gmail.com>
2024-05-22 01:23:51 +0200

    123

--- UI/Forms/CasePage/AddServiceView.cs
+++ UI/Forms/CasePage/AddServiceView.cs
@@ -178,11 +178,11 @@ namespace UI.Forms.CasePage
             bool succes = await caseServiceBL.CreateCaseServiceAsync(caseServiceUI);
             if (succes)
             {
-                MessageBox.Show("Du er dejlig Rasmus");
+                MessageBox.Show("Service has been added");
             }
             else
             {
-                MessageBox.Show("Du dum");
+                MessageBox.Show("Failed to add the service");
             }
 
             await caseDetailsView.SetDgvAsync();
